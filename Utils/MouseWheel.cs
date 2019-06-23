fbc3bf174edcdc64cc4b965f770debf53cb34fc6
しょうちゃん <36469195+sh-01@users.noreply.github.com>
2019-06-23 19:29:36 +0900

    Create MouseWheel.cs

---
+++ Utils/MouseWheel.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWheel : MonoBehaviour
+{
+    public static bool ScrollUp;
+    public static bool ScrollDown;
+    private void Update()
+    {
+        if (ScrollUp){ScrollUp = false;}
+        if (ScrollDown){ScrollDown = false;}
+        float scroll = Input.GetAxis("Mouse ScrollWheel") * 10;
+        if (0 < scroll){ScrollDown = false;ScrollUp = true;} /* ScrollUp */
+        if (0 > scroll){ScrollUp = false;ScrollDown = true;} /* ScrollDown */
+    }
+}
+
+
+## IN MAIN CLASS ##
+
+public class Main : Monobehavior
+{
+    void Upsate()
+    {
+        if (MouseWheel.ScrollUp)
+        {
+            print("Scroll up! and Zoom in!");
+        }
+        
+        if (MouseWheel.ScrollDown)
+        {
+            print("Scroll down! and Zoom out!");
+        }
+    }
+}
